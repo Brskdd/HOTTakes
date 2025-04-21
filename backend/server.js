03b09a32505d15208328537707b2f68bb2a618a2
Brskdd <134308760+Brskdd@users.noreply.github.com>
2025-04-21 11:50:08 -0400

    more updates

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const fs = require('fs')
+
+const datapath = "../database/users.json"
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -9,6 +12,40 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.post("/createuser", async (req, res) => {
+
+    const { name, password } = req.body
+
+    if (!name || !password ) {
+        return res.status(400).json({ error: "fill out the whole form" })
+    }
+
+    try {
+        let users = []
+
+        if (fs.existsSync(datapath)) {
+            const data = fs.readFileSync(datapath, 'utf8')
+            users = JSON.parse(data)
+        }
+
+        if (users.some(user => user.name === name)) {
+            return res.status(400).json({ error: "user already exists" })
+        }
+
+        const hashpass = await bcrypt.hash(password, 10)
+
+        const newuser = { name, money: 20, password: hashpass }
+        users.push(newuser)
+
+        fs.writeFileSync(datapath, JSON.stringify(users, null, 2), 'utf8')
+
+        res.status(201).json({ message: "user created" })
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 // Start the server
 app.listen(port, () => {
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+function Dashboard() {
+  return (
+    <div>
+      <p>AAAAAAAAAAAAAAAAAAA</p>
+      <p>AAAAAAAAAAAAAAAAAAA</p>
+      <p>AAAAAAAAAAAAAAAAAAA</p>
+      <p>AAAAAAAAAAAAAAAAAAA</p>
+      <p>AAAAAAAAAAAAAAAAAAA</p>
+      <p>AAAAAAAAAAAAAAAAAAA</p>
+      <p>AAAAAAAAAAAAAAAAAAA</p>
+    </div>
+  );
+}
+
+export default Dashboard;
