cc57762ae08e8fc4ea93af61be7d47b015f7451e
Brskdd <yeetboi1229+1@gmail.com>
2025-04-22 23:02:37 -0400

    half baked login form not even implemented (ACTUALLY CHECK TMRW TO SEE THAT IT EVEN WORKS ON THE FRONTEND)

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,19 @@
+function LoginForm() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+export default LoginForm;
